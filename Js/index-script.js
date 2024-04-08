a5a1d855c02eca8d7de0d46d85c18a3f4e95fe7c
KP <kalvin.paviel@epitech.eu>
2024-04-08 14:44:40 +0200

    [+] Cards en 4 + randoms

--- Js/index-script.js
+++ Js/index-script.js
@@ -1,22 +1,31 @@
-// Fonction pour charger les données JSON et générer les cartes de recettes
+// Fonction pour mélanger aléatoirement un tableau
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 async function afficherRecettes() {
     const response = await fetch('./data.json');
     const data = await response.json();
     const recettesContainer = document.getElementById('recettesContainer');
-    data.recettes.forEach(recette => {
+    const recettesMelangees = shuffle(data.recettes);
+    const nombreDeRecettes = 4;
+    for (let i = 0; i < nombreDeRecettes; i++) {
+        const recette = recettesMelangees[i];
         const card = document.createElement('div');
         card.classList.add('col-md-6', 'mb-4');
         card.innerHTML = `
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">${recette.nom}</h5>
-                    <p class="card-text">Temps de préparation: ${recette.temps_preparation}</p>
                     <p class="card-text">Catégorie: ${recette.categorie}</p>
                     <a href="#" class="btn btn-primary">Voir la recette</a>
                 </div>
             </div>
         `;
         recettesContainer.appendChild(card);
-    });
+    }
 }
 window.onload = afficherRecettes;
