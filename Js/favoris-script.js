0bbcf113fdef224ff83493863b9c175cc9dbdabd
KP <kalvin.paviel@epitech.eu>
2024-04-10 17:03:54 +0200

    [+] ajout bouton calendrier

--- Js/favoris-script.js
+++ Js/favoris-script.js
@@ -1,4 +1,3 @@
-//Lorsque la page est chargée, le code récupère les recettes que l'utilisateur a ajoutées à ses favoris auparavant, à partir de la mémoire de son navigateur. Ensuite, il affiche ces recettes sur la page web dans une liste, chacune avec un bouton pour les supprimer. i l'utilisateur décide de supprimer une recette de ses favoris en cliquant sur le bouton correspondant, cette recette est retirée de la liste affichée sur la page. Après avoir supprimé une recette de ses favoris, la liste mise à jour est sauvegardée dans la mémoire du navigateur de l'utilisateur pour que la prochaine fois qu'il visite la page, il voie la liste actualisée.
 document.addEventListener("DOMContentLoaded", function() {
     const favoritesList = document.getElementById("favoritesList");
     const favoris = JSON.parse(localStorage.getItem("favoris")) || [];
@@ -10,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 <h5 class="card-title">${recette.title}</h5>
                 <p class="card-text">${recette.body}</p>
                 <button class="btn btn-danger delete-btn" data-index="${index}">Supprimer</button>
+                <button class="btn btn-primary second-btn">Deuxième bouton</button>
             </div>
         `;
         favoritesList.appendChild(recetteCard);
@@ -29,6 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         <h5 class="card-title">${recette.title}</h5>
                         <p class="card-text">${recette.body}</p>
                         <button class="btn btn-danger delete-btn" data-index="${index}">Supprimer</button>
+                        <button class="btn btn-primary second-btn">Deuxième bouton</button>
                     </div>
                 `;
                 favoritesList.appendChild(recetteCard);
